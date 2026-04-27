2aa31ec2f4343f47589586d253d2626a5ebbe88e
kiskukta <kiskuktaa20@gmail.com>
2026-04-27 18:01:29 +0200

    Adatbázis class-jainak megcsinálása

--- Dnn.Kiskukta.Module/Models/UserRecipePostInfo.cs
+++ Dnn.Kiskukta.Module/Models/UserRecipePostInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,41 @@ namespace Dnn.Kiskukta.Dnn.Kiskukta.Module.Models
 {
 	public class UserRecipePostInfo
 	{
-	}
+        public int PostId { get; set; }
+
+        public int ModuleId { get; set; }
+
+        public string RecipeName { get; set; }
+
+        public string CommentText { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public int CreatedByUserId { get; set; }
+
+        public string CreatedByDisplayName { get; set; }
+
+        public DateTime CreatedOnDate { get; set; }
+
+        public string Status { get; set; }
+
+        [IgnoreColumn]
+        public string StatusDisplay
+        {
+            get
+            {
+                switch ((Status ?? "").Trim().ToLower())
+                {
+                    case "approved":
+                        return "Megjelenítve";
+                    case "pending":
+                        return "Függőben";
+                    case "rejected":
+                        return "Elrejtve";
+                    default:
+                        return Status;
+                }
+            }
+        }
+    }
 }
