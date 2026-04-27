eb72449a667e107cb14c6882fa5d4673d0d249e2
kiskukta <kiskuktaa20@gmail.com>
2026-04-27 17:59:36 +0200

    Új projekt, adatbázis megcsinálása, teszt view szerkesztése, hogy működik-e

---
+++ Dnn.Kiskukta.Module/Controllers/UserRecipePostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dnn.Kiskukta.Dnn.Kiskukta.Module.Controllers
+{
+    public class UserRecipePostController : Controller
+    {
+        // GET: UserRecipePost
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dnn.Kiskukta.Module/Models/UserRecipePostInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnn.Kiskukta.Dnn.Kiskukta.Module.Models
+{
+	public class UserRecipePostInfo
+	{
+	}
+}
